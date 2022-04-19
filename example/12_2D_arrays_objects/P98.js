1cc36aeb442c6638986ed0fea01f98a719d65e9d
김 은영 <michelle@Neowiz.com>
2022-04-19 18:40:46 +0900

    12주차

--- example/12_2D_arrays_objects/P98.js
+++ example/12_2D_arrays_objects/P98.js
@@ -5,3 +5,18 @@ import { question } from 'readline-sync';
 
 // 문제98) 096번 프로그램의 2차원 배열을 이용하여 사용자에게 표시할 행을 요청하고 그 행을 출력한다.
 // 새로운 값을 입력하라고 요청하고 그것을 표시한 행에 추가하고 그 행을 다시 출력하라.
+
+const array = [
+  [2, 5, 8],
+  [3, 7, 4],
+  [1, 6, 9],
+  [4, 2, 0],
+];
+
+const row = questionInt('표시할 행 입력: ');
+console.log(array[row]);
+
+const newRow1 = questionInt('새로운 값 입력: ');
+const newRow2 = array[row].join(',');
+
+console.log(`${newRow2},${newRow1}`);
